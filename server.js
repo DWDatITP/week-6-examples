0b317a0517b2edd2a248295cedc7e28c70524d8a
Mike Dory <mike@dory.me>
2014-04-30 19:41:10 -0400

    adding express3-handlebars

--- server.js
+++ server.js
@@ -1,10 +1,26 @@
 var express = require('express');
+var exphbs  = require('express3-handlebars');
+
 var app = express();
 
 app.set('port', process.env.PORT || 3000);
 
+app.set('views', __dirname+'/views');
+app.use(express.static(__dirname + '/public'));
+
+hbs = exphbs.create({
+  defaultLayout: 'default',
+  extname: '.html'
+});
+
+app.engine('html', hbs.engine);
+app.set('view engine', 'html');
+
+
 app.get('/', function (req,res) {
-  res.send(200);
+  res.render('index', {
+    "title" : "hi!"
+  });
 });
 
 app.listen(app.get('port'));
