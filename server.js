b49943ff36470c7a6cd819408c1dfccc0143c2d8
Mike Dory <mike@dory.me>
2014-04-30 20:45:54 -0400

    adding models. for science.

--- server.js
+++ server.js
@@ -2,7 +2,20 @@ var express = require('express');
 var exphbs  = require('express3-handlebars');
 var app = express();
 
+app.use(express.json());
+app.use(express.urlencoded());
 app.set('port', process.env.PORT || 3000);
+
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/classsix');
+
+var Person = mongoose.model('Person', {
+  name: String,
+  age: Number,
+  dateAdded: Date,
+  active: Boolean
+});
+
 
 app.set('views', __dirname+'/views');
 app.use(express.static(__dirname + '/public'));
@@ -17,8 +30,35 @@ app.set('view engine', 'html');
 
 
 app.get('/', function (req,res) {
-  res.render('index', {
-    "title" : "hi!"
+  Person.find({"active":true}, function (err, data) {
+    console.log(data);
+    res.render('index', {
+      "title" : "hi!",
+      "people":data
+    });
+  });
+});
+
+// create a new person
+app.post('/person', function (req, res) {
+  console.log(req.body);
+
+  var personData = {
+    name : req.body.name,
+    age: req.body.age,
+    dateAdded : Date(),
+    active: true
+  };
+
+  p = new Person(personData);
+  p.save(function (err, person) {
+    if (err) {
+      console.error(err);
+      res.status(500).json({status: "Error!"});
+    } else {
+      console.log(person);
+      res.status(200).json({status:"Success!"});
+    }
   });
 });
 
